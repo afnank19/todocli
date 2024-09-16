5df0d0822bc3f97d06953ae08085d3bb50352e9d
Afnan Khalid <afnank19@gmail.com>
2024-09-17 03:25:57 +0500

    add sql func, refactor list code

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"io"
 	"os"
@@ -13,8 +14,32 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
+var db *sql.DB
+
 func main() {
-	p := tea.NewProgram(initialModel(), tea.WithAltScreen())
+	var err error
+	db, err = sql.Open("sqlite3", "./todo.db")
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
+
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS projects (ID INTEGER PRIMARY KEY AUTOINCREMENT, projectName TEXT NOT NULL);")
+	if err != nil {
+		panic(err)
+	}
+
+	//Test Code, delete db before running
+	// insertQuery := "INSERT INTO projects(projectName) VALUES (?)"
+	// _, err = db.Exec(insertQuery, "Getting Started")
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	initialItems := InitProjectList(db)
+
+	p := tea.NewProgram(initialModel(initialItems), tea.WithAltScreen())
 
 	if _, err := p.Run(); err != nil {
 		fmt.Fprintf(os.Stderr, "Error: %v", err)
@@ -33,6 +58,7 @@ var (
 	paginationStyle   = list.DefaultStyles().PaginationStyle.PaddingLeft(4)
 	helpStyle         = list.DefaultStyles().HelpStyle.PaddingLeft(4).PaddingBottom(1)
 	doneStyle         = lipgloss.NewStyle().Foreground(lipgloss.Color("#4a4a4a")).Strikethrough(true).PaddingLeft(4)
+	title             = lipgloss.NewStyle().Bold(true).Foreground(lipgloss.Color("#d4c6a9")).MarginLeft(2)
 	//centered          = lipgloss.NewStyle().Align(lipgloss.Center) //This needs a .Width with terminal width to appropriately center
 	//backgroundStyle   = lipgloss.NewStyle().Background(lipgloss.Color("201"))
 	//quitTextStyle     = lipgloss.NewStyle().Margin(1, 0, 2, 4)
@@ -77,25 +103,54 @@ func (d itemDelegate) Render(w io.Writer, m list.Model, index int, listItem list
 }
 
 type model struct {
-	tasks      []string
-	textInput  textinput.Model
-	list       list.Model
-	addingTask bool
-	items      []list.Item
-	done       []bool
-	taskList   list.Model
-	taskItems  []list.Item
-	taskView   bool
-	choice     string
+	textInput     textinput.Model
+	taskInput     textinput.Model
+	list          list.Model
+	taskList      list.Model
+	items         []list.Item
+	taskItems     []list.Item
+	tasks         []string
+	choice        string
+	done          []bool
+	addingProject bool
+	taskView      bool
+	addingTask    bool
 }
 
-func initialModel() model {
+func InitProjectList(db *sql.DB) []list.Item {
+	rows, err := db.Query("SELECT ID, projectName from projects")
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	var items []list.Item
+
+	for rows.Next() {
+		var ID string
+		var projName string
+		err = rows.Scan(&ID, &projName)
+		if err != nil {
+			panic(err)
+		}
+		items = append(items, item(projName))
+	}
+
+	return items
+}
+
+func initialModel(initialItems []list.Item) model {
 	ti := textinput.New()
 	ti.Placeholder = "Type something"
 	ti.CharLimit = 100
 	ti.Width = 20
 
-	items := []list.Item{}
+	taskIn := textinput.New()
+	taskIn.Placeholder = "Enter a task"
+	taskIn.CharLimit = 100
+	taskIn.Width = 40
+
+	items := initialItems
 
 	const defaultWidth = 20
 
@@ -103,19 +158,21 @@ func initialModel() model {
 	l.Title = "Project List"
 	l.SetShowStatusBar(false)
 	l.SetFilteringEnabled(false)
-	l.Styles.Title = titleStyle
+	l.Styles.Title = title
 	l.Styles.PaginationStyle = paginationStyle
 	l.Styles.HelpStyle = helpStyle
 
 	return model{
-		tasks:      []string{"Test"},
-		addingTask: false,
-		textInput:  ti,
-		list:       l,
-		items:      items,
-		done:       []bool{true},
-		taskView:   false,
-		choice:     "",
+		tasks:         []string{"Test"},
+		addingProject: false,
+		textInput:     ti,
+		list:          l,
+		items:         items,
+		done:          []bool{true},
+		taskView:      false,
+		choice:        "",
+		addingTask:    false,
+		taskInput:     taskIn,
 	}
 }
 
@@ -133,13 +190,19 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		return m, nil
 
 	case tea.KeyMsg:
-		if msg.String() == "ctrl+a" && !m.addingTask {
+		//This is for adding a new project
+		if msg.String() == "ctrl+a" && !m.addingProject && !m.taskView {
 			m.textInput.Focus()
+			m.addingProject = true
+			return m, textinput.Blink
+		}
+		if msg.String() == "ctrl+a" && !m.addingProject && m.taskView {
+			m.taskInput.Focus()
 			m.addingTask = true
 			return m, textinput.Blink
 		}
 
-		if m.taskView {
+		if m.taskView && !m.addingTask {
 			switch msg.String() {
 			case "enter":
 				idx := m.list.Index()
@@ -157,17 +220,48 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 				items := []list.Item{item("Project 1"), item("Lewis")} //Probably a database call to get the project list
 				m.list.SetItems(items)
 				m.taskView = false
+
+			case "delete":
+				idx := m.list.Index()
+				m.list.RemoveItem(idx)
 			}
 
 			var cmd tea.Cmd
 			m.list, cmd = m.list.Update(msg)
 			return m, cmd
 		}
+		if m.addingTask && m.taskView {
+			switch msg.String() {
+			case "enter":
+				task := strings.TrimSpace(m.taskInput.Value())
+				if task != "" {
+					m.tasks = append(m.tasks, task)
+					m.items = append(m.items, item(task))
+					m.list.InsertItem(len(m.items)+1, item(task))
+				}
+				// Clear input and return to normal mode
+				m.taskInput.Reset()
+				m.addingTask = false
 
-		if m.addingTask {
+			case "tab":
+				m.taskInput.Reset()
+				m.addingTask = false
+			}
+			m.taskInput, cmd = m.taskInput.Update(msg)
+			return m, cmd
+		}
+
+		if m.addingProject {
 			switch msg.String() {
 			case "enter":
 				task := strings.TrimSpace(m.textInput.Value())
+
+				insertQuery := "INSERT INTO projects(projectName) VALUES (?)"
+				_, err := db.Exec(insertQuery, task)
+				if err != nil {
+					panic(err)
+				}
+
 				if task != "" {
 					m.tasks = append(m.tasks, task)
 					m.items = append(m.items, item(task))
@@ -175,17 +269,17 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 				}
 				// Clear input and return to normal mode
 				m.textInput.Reset()
-				m.addingTask = false
+				m.addingProject = false
 
-			case "esc":
+			case "tab":
 				m.textInput.Reset()
-				m.addingTask = false
+				m.addingProject = false
 			}
 			m.textInput, cmd = m.textInput.Update(msg)
 			return m, cmd
 		}
 
-		if !m.addingTask && !m.taskView {
+		if !m.addingProject && !m.taskView && !m.addingTask {
 			switch msg.String() {
 			case "enter":
 				//m.taskView = true
@@ -223,7 +317,7 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			return m, cmd
 		}
 
-		if (msg.String() == "q" || msg.String() == "ctrl+c") && !m.addingTask {
+		if (msg.String() == "q" || msg.String() == "ctrl+c") && !m.addingProject {
 			return m, tea.Quit
 		}
 
@@ -232,8 +326,12 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (m model) View() string {
+	if m.addingProject {
+		return fmt.Sprintf("Add a new project: %s\n\nPress Enter to submit or Tab to cancel.", m.textInput.View())
+	}
+
 	if m.addingTask {
-		return fmt.Sprintf("Add a new task: %s\n\nPress Enter to submit or Esc to cancel.", m.textInput.View())
+		return fmt.Sprintf("Add a new task: %s\n\nPress Enter to submit or Tab to cancel.", m.taskInput.View())
 	}
 
 	// if m.taskView {
@@ -245,7 +343,7 @@ func (m model) View() string {
 	//listView := backgroundStyle.Render(m.list.View()) experiment
 	//listView := centered.Render(m.list.View()) //Uncomment for center
 
-	if !m.addingTask {
+	if !m.addingProject {
 		return "\n\n" + m.list.View() + "\n\n" + help
 	}
 
